e870fc6a7fea9f6bbdf0c7778a85d336e042b4fd
Maxim Belov <belovmd@gmail.com>
2021-05-13 00:29:09 +0300

    Iteration 1. Add model, add model to admin

---
+++ lesson/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+# admin.site.register(models.Material)
+
+@admin.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'material_type', 'publish')
+    list_filter = ('created_at', 'updated_at', 'material_type')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    date_hierarchy = 'publish'
+    ordering = ('material_type', 'publish')
---
+++ lesson/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Material(models.Model):
+    MATERIAL_TYPE = (
+        ('theory', 'Theoretical Material'),
+        ('practice', 'Practical Marterial'),
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish')
+    body = models.TextField()
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    publish = models.DateTimeField(default=timezone.now)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='user_materials')
+    material_type = models.CharField(max_length=20,
+                                     choices=MATERIAL_TYPE,
+                                     default='theory')
+
+    def __str__(self):
+        return self.title
