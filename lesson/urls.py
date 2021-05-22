5c9d0218f0c67ace2e7a72f118581877d97b7d0c
Maxim Belov <belovmd@gmail.com>
2021-05-22 12:17:40 +0300

    Iteration 8. Add password reset

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 app_name = 'lesson'
+
+
+class MyHack(auth_views.PasswordResetView):
+    success_url = reverse_lazy('lesson:password_reset_done')
+
 
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
@@ -14,4 +20,11 @@ urlpatterns = [
     # path('login/', views.user_login, name='user_login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_reset/', MyHack.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('lesson:password_reset_complete'),
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
