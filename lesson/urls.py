320e6a1b6dcb58063a774db0bb31d770035b9872
Maxim Belov <belovmd@gmail.com>
2021-05-22 11:36:04 +0300

    Iteration 7. Add django login

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'lesson'
@@ -11,5 +11,7 @@ urlpatterns = [
     path('<int:material_id>/share/', views.share_material,
          name='share_material'),
     path('create/', views.create_material, name='create_material'),
-    path('login/', views.user_login, name='user_login'),
+    # path('login/', views.user_login, name='user_login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
